fb6f830ddc0f438f2697991684a35164d62f1403
John Prince <jtprince@gmail.com>
2013-05-13 13:05:30 -0600

    jtp and huan revisions

--- hash_peptide_anno.rb
+++ hash_peptide_anno.rb
@@ -1,47 +1,66 @@
+#!/usr/bin/env ruby
+
 require 'yaml'
+
 ext = ".peptide_to_annot.yml"
-if ARGV.size == 0
- puts"usage: #{File.basename($0)} <file>.yml(peptide_to_protIDs) <file>.tsv(protIDs_to_annotion)"
- puts "output: <file#{ext}>.yml(peptide_to_annotations)"
+
+if ARGV.size != 2
+ puts "usage: #{File.basename($0)} <file>.yml(pep_to_protIDs) <file>.tsv(protIDs_to_annot)"
+ puts "output: <file>#{ext}"
  exit
 end
 
-peptide_to_proIDs_file = ARGV[0]
-proIDs_to_annot_file = ARGV[1]
-output = YAML::load(File.open(peptide_to_proIDs_file))
-peptide_to_ID_hash = Hash.new
+(pep_to_prot_file, prot_to_annot_file) = ARGV
 
-output.each do |peptide, protIDs|
-  prot = protIDs.split("\t")
-    prot.each do |prot_id|
-	peptide_to_ID_hash [peptide] = prot_id
+def get_pep_to_prot(file)
+  pep_to_prot = YAML.load_file(pep_to_prot_file)
+
+  def pep_to_prot.prot_ids(peptide)
+    prot_s = self[peptide]
+    if prot_s  # might be nil
+      prot_s.split("\t")
+    else
+      nil
     end
+  end
+  pep_to_prot
 end
 
-protIDs_to_annot_hash = Hash.new
-IO.foreach(proIDs_to_annot_file) do |line| 
-
- (data, *anno) = line.split("\t")		
-  if protIDs_to_annot_hash.key?(data)
-    protIDs_to_annot_hash[data] << anno[3]+"\t"  
-  else
-    protIDs_to_annot_hash[data] = anno[3]+ "\t" 
-  end 
+def get_prot_to_annot(file)
+  prot_to_annot = Hash.new {|h,k| h[k] = [] }
+  File.open(file) do |io|
+    header = io.readline
+    io.each_line.with_index do |line,i|
+      (prot_id, *anno) = line.split("\t")		
+      prot_to_annot[prot_id] << anno[2]
+      if (i % 10000) == 0
+        print '.' ; $stdout.flush
+      end
+    end
+  end
+  prot_to_annot
 end
 
+prot_to_annot = get_prot_to_annot(prot_to_annot_file)
+p prot_to_annot.size
+abort 'here'
 
-peptide_to_ID_hash.each_key do |key| 
- if  protIDs_to_annot_hash.key? peptide_to_ID_hash[key]    
-   data = peptide_to_ID_hash[key] 
-   peptide_to_ID_hash[key] = protIDs_to_annot_hash[data]		
- else 			
-   peptide_to_ID_hash.delete(key)
- end
-end
-	
-file = ARGV[1]
-base = File.basename(file).split(".")[0]
+pep_to_prot = get_pep_to_prot(pep_to_prot_file)
+
+base = pep_to_prot_file.chomp(File.extname(pep_to_prot_file))
 outfile = base + ext
-File.open(outfile, "w") do |file|
- file.write peptide_to_ID_hash.to_yaml
+
+File.open(outfile, "w") do |out|
+
+  pep_to_prot.keys.each do |pep| 
+
+    pep_to_prot.prot_ids(pep).each do |prot_id|
+
+      if  prot_to_annot.pep?(pep_to_prot[pep])
+        out.puts "#{pep}: #{prot_to_annot[prot_id].join("\t")}"
+      end
+
+    end
+  end
+
 end
