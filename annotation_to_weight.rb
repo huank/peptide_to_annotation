32e5b0fdf25994839b010be9f106f175f1b8dc72
John Prince <jtprince@gmail.com>
2013-05-17 16:09:56 -0600

    some alterations with Dr. Prince

--- annotation_to_weight.rb
+++ annotation_to_weight.rb
@@ -1,67 +1,62 @@
 require 'yaml'
 ext = ".annotation_to_weight.yml"
-if ARGV.size != 2
- puts "usage: #{File.basename($0)} <file>.tsv(Proeome Discoverer default export) <file>.tsv(peptide_to_annot_database)"
- puts "output: <file>#{ext}"
- exit
+
+def tell_how_long(&block)
+  puts "starting to time now!" ; $stdout.flush
+  start = Time.now
+  reply = block.call
+  puts "Took #{Time.now - start} seconds"
+  reply
 end
-default_export_file = ARGV[0]
-Peptide_to_annotation_db = ARGV[1]
+
+if ARGV.size != 2
+  puts "usage: #{File.basename($0)} <file>.tsv(Proeome Discoverer default export) <file>.tsv(peptide_to_annot_database)"
+  puts "output: <file>#{ext}"
+  exit
+end
+
+(default_export_file, peptide_to_annotation_db) = ARGV
 
 base = pep_to_prot_file.chomp(File.extname(default_export_file))
-
 outfile = base + ext
 
+peptide_with_PSM = Hash.new {|h,k| h[k] = 0 }
 
-peptide_with_PSM = Hash.new #{|h,k| h[k] = []}
-sequence = Array.new
- File.open(default_export_file) do |io|
- header = io.readline
+File.open(default_export_file) do |io|
+  header = io.readline
   io.each_line do |line|
-  pep = line.split("\t")
-	seq = pep[3].split("\"")
-	seq = seq[1]
-   
-	 sequence << seq	 
-	if peptide_with_PSM.key? seq   
-	 peptide_with_PSM [seq] = peptide_with_PSM [seq] +1
-	else
-	peptide_with_PSM [seq] = 1		 
-	end
- end
+    data = line.split("\t")
+    seq = data[3].split('"').last
+    peptide_with_PSM[seq] += 1
+  end
 end
-#puts peptide_with_PSM["VGIENIGR"]  
-tic = sequence.size.to_f
 
-annotation_to_weight = Hash.new
-peptide_with_PSM.each_key do |search|
+tell_how_long {
+  peptide_to_annot = {}
+  IO.foreach(peptide_to_annotation_db) do |line|
+    line.chomp!
+    peptide_to_annot.store( *line.split(': ') ) # storing value as a string!
+  end
+}
 
-IO.foreach(Peptide_to_annotation_db) do |line|
- (pep, *goannots) = line.split(": ")
- annot = goannots[0].split("\n")
- annot = annot[0].split("\t") 
- 
- m = annot.size.to_f
- weight_of_annotation = 1/m
- 
- n = 0 
- 
- if search == pep   #some peptides have modification in there
- while n <annot.size
-    annotation = annot[n] 
-    psm = peptide_with_PSM[search].to_f / tic
-	
-	if annotation_to_weight.key? annotation
-	annotation_to_weight[annotation] = annotation_to_weight[annotation] + weight_of_annotation * psm
-	else
-	annotation_to_weight[annotation] = weight_of_annotation* psm
-	end
-	n = n+1
+tell_how_long {
+  total_psms = peptide_with_PSM.values.reduce(:+)
+}
+
+annotation_to_weight = Hash.new {|h,k| h[k] = 0.0 }
+
+peptide_with_PSM.each do |query_peptide, spectral_hit_cnt|
+
+  annotations = peptide_to_annot[query_peptide].split("\t")
+  weight_of_annotation = 1.0 / annotations.size
+
+  annotations.each do |annotation|
+
+    nrmlzed_psm_cnt = spectral_hit_cnt.to_f / total_psms
+
+    annotation_to_weight[annotation] += (weight_of_annotation * nrmlzed_psm_cnt)
+  end
 end
-puts "."
+
 File.write(outfile, annotation_to_weight.to_yaml)
- end
 
-end
-
-end
